69d855375d70d1ae99677fbc931d7854084e84ad
Shubhang Gupta <guptashubhang2000@gmail.com>
2023-10-07 03:13:37 +0530

    workpage style

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -22,7 +22,6 @@ export const Work = [
     techStack: findIconsData([
       "React",
       "Redux",
-      "Canva",
       "Javascript",
       "Express",
       "Nodemon",
@@ -30,52 +29,69 @@ export const Work = [
     githubUrl: "https://github.com/shubhanggupta2000/proshop_ecommerce",
     hostlink: "https://proshop-2.onrender.com/",
     imagePath:
-      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1685812718/proshop_jflwbz.png",
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625027/nr0qyprtltvklibju6mb.png",
   },
   {
     index: 2,
+    name: "SushiMan",
+    info: "A captivating Sushi ordering landing page featuring custom animations, a stylish navigation bar, and a unique hero section",
+    techStack: findIconsData([
+      "React",
+      "Redux",
+      "Canva",
+      "Javascript",
+      "Express",
+      "Nodemon",
+    ]),
+    githubUrl: "https://github.com/shubhanggupta2000/sushiman",
+    hostlink: "https://sushiman-app.vercel.app/",
+    imagePath:
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625027/ck2gj2hqtt3bsfe4okbs.png",
+  },
+  {
+    index: 3,
     name: "Gericht Restaurant",
     info: "A modern UI/UX restaurant landing page build with fundamental CSS properties with master flex & grid.",
     techStack: findIconsData(["React", "Javascript", "HTML", "CSS", "Netlify"]),
     githubUrl: "https://github.com/shubhanggupta2000/Restaurant_UI",
     hostlink: "https://fazevennom-gericht.netlify.app/",
     imagePath:
-      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1685812719/gericht_oesnyv.png",
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625029/vou4msfeaeehjbncuwor.png",
   },
   {
-    index: 3,
+    index: 4,
     name: "YouTube 2.0",
     info: "A simple video streaming YouTube clone build with youtube API and MUI-5.",
     techStack: findIconsData(["React", "Material UI", "Axios", "Netlify"]),
     githubUrl: "https://github.com/shubhanggupta2000/youtube-clone",
     hostlink: "https://fazevennom-youtubeclone.netlify.app/",
     imagePath:
-      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1685812687/youtube_k5zfq3.png",
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625031/cfrp0hr81dunqm47v04s.png",
   },
   {
-    index: 4,
+    index: 5,
     name: "Closet",
     info: "An Ecommerce website with Sass and mobile first approach.",
     techStack: findIconsData(["React", "Material UI", "Netlify", "Sass"]),
     githubUrl: "https://github.com/shubhanggupta2000/Closet",
     hostlink: "https://fazevennom-closet-ecommerce.netlify.app/",
     imagePath:
-      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1685812717/closet_xsbrwy.jpg",
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625026/oujhtdvtmfk2lg1wtm76.png",
   },
   {
-    index: 5,
-    name: "Shopify - Gadgets ecommerce",
-    info: "A Sound gadget shop with stripe payment Integration.",
+    index: 6,
+    name: "Shopify Gadgets",
+    info: "A Sound gadget shop with stripe payment integration & confetti effects.",
     techStack: findIconsData(["Three.js", "Javascript"]),
     githubUrl: "https://github.com/shubhanggupta2000/ecommerce",
     hostlink: "https://fazevennom-shoppify.vercel.app/",
     imagePath:
-      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1685812719/ecommerce_oupeeu.png",
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625028/guwuk0lekuflvhd8eto6.png",
   },
   {
-    index: 6,
+    index: 7,
     name: "Golds Gym",
-    info: "Fitness app with the functionality to choose exercise categories and specific muscle groups.",
+    info: "Fitness app with the functionality to choose exercise categories and specific muscle groups along with motion cards GIF.",
     techStack: findIconsData([
       "React",
       "Node",
@@ -86,10 +102,10 @@ export const Work = [
     githubUrl: "https://github.com/shubhanggupta2000/Golds-gym",
     hostlink: "https://fazevennom-gym.netlify.app/",
     imagePath:
-      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1685812718/gym_htpv79.png",
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625026/mu79mdpswechuzevxxhl.png",
   },
   {
-    index: 7,
+    index: 8,
     name: "Todo App",
     info: "A simple yet good looking todo list web-app.",
     techStack: findIconsData([
@@ -102,6 +118,70 @@ export const Work = [
     githubUrl: "https://github.com/shubhanggupta2000/todo",
     hostlink: "https://fazevennom-todo.vercel.app/",
     imagePath:
-      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1685812718/todo_rjqtcs.png",
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625028/gnutpx6mau9ztupwkzya.png",
+  },
+  {
+    index: 9,
+    name: "Evolux",
+    info: "A car rental website with smooth scrolling, captivating animations, and a delightful user interface.",
+    techStack: findIconsData([
+      "React",
+      "Node",
+      "Mongodb",
+      "Heroku",
+      "Postgres",
+    ]),
+    githubUrl: "https://github.com/shubhanggupta2000/evolux-car-website",
+    hostlink: "https://evolux-car-website.vercel.app/",
+    imagePath:
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625026/pdz8kwh5tqqpvpsvwker.png",
+  },
+  {
+    index: 10,
+    name: "Movies Info",
+    info: "A simple web-app that provides a brief overview of any of your favourite movies.",
+    techStack: findIconsData([
+      "React",
+      "Node",
+      "Mongodb",
+      "Heroku",
+      "Postgres",
+    ]),
+    githubUrl: "https://github.com/shubhanggupta2000/movies-info",
+    hostlink: "https://fazevennom-movies-info.netlify.app/",
+    imagePath:
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625028/f6m9uylrerinpwc1ecoq.png",
+  },
+  {
+    index: 11,
+    name: "Quiz App",
+    info: "An amazing quiz application build with React & Semantic-UI that lets you play fully customized quiz in your browser.",
+    techStack: findIconsData([
+      "React",
+      "Node",
+      "Mongodb",
+      "Heroku",
+      "Postgres",
+    ]),
+    githubUrl: "https://github.com/shubhanggupta2000/quiz-app",
+    hostlink: "https://fazevennom-quiz-app.vercel.app/",
+    imagePath:
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625028/zbpum6fxvo0avriews2v.png",
+  },
+  {
+    index: 12,
+    name: "Shoppy Dashboard",
+    info: "An admin panel with Dashboard, calender with reminders and many more features.",
+    techStack: findIconsData([
+      "React",
+      "Node",
+      "Mongodb",
+      "Heroku",
+      "Postgres",
+    ]),
+    githubUrl: "https://github.com/shubhanggupta2000/syncfusion-dashboard",
+    hostlink: "https://fazevennom-syncfusion-dashboard.netlify.app/",
+    imagePath:
+      "https://res.cloudinary.com/ddwqxi6zp/image/upload/v1696625025/tz8kyqkssinfusokd7iv.png",
   },
 ];
--- src/subComponents/Card.js
+++ src/subComponents/Card.js
@@ -1,15 +1,17 @@
 // import { motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
+import { Tilt } from "react-tilt";
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: ${(props) => (props.index % 2 === 0 ? "row-reverse" : "row")};
   height: fit-content;
   width: 100%;
   color: white;
   justify-content: space-around;
   align-items: center;
+  margin-top: 3rem;
 `;
 
 const Right = styled.div`
@@ -27,29 +29,25 @@ const Left = styled.div`
   align-items: center;
 `;
 
-// Framer motion configuration
-// const Item = {
-//   hidden: {
-//     scale: 0,
-//   },
-//   show: {
-//     scale: 1,
-//     transition: {
-//       type: "spring",
-//       duration: 0.5,
-//     },
-//   },
-// };
+const ImageContainer = styled.div``;
 
 const Card = (props) => {
-  const { index, name, info, hostlink, githubUrl, imagePath } = props.data;
+  const { index, name, info, hostlink, githubUrl, imagePath, techStack } =
+    props.data;
 
   return (
     <>
-      <Container>
+      <Container index={index}>
         <Left>
           <h1>{name}</h1>
           <h3>{info}</h3>
+          {techStack.map((items) => {
+            return (
+              <div>
+                <img src={items.icon} alt="" />
+              </div>
+            );
+          })}
           <span>
             <a href={hostlink}>
               <button>See Live</button>
@@ -60,7 +58,11 @@ const Card = (props) => {
           </span>
         </Left>
         <Right>
-          <img src={imagePath} width="100%" alt="" />
+          <ImageContainer>
+            <Tilt tiltMaxAngleX={0.5} tiltMaxAngleY={0.5} scale={1}>
+              <img src={imagePath} width="100%" alt="" />
+            </Tilt>
+          </ImageContainer>
         </Right>
       </Container>
     </>
